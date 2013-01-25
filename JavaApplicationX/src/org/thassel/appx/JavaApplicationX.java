4f5314a0fcc07bcb552cff424bd8ac37af5c83a3
thassel <t.hassel47@gmail.com>
2013-01-25 15:36:22 +0100

    Added some "code"

--- JavaApplicationX/src/org/thassel/appx/JavaApplicationX.java
+++ JavaApplicationX/src/org/thassel/appx/JavaApplicationX.java
@@ -1,7 +1,4 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package org.thassel.appx;
 
 /**
@@ -14,6 +11,9 @@ public class JavaApplicationX {
    * @param args the command line arguments
    */
   public static void main(String[] args) {
-    // TODO code application logic here
+    
+    
+    System.out.println("lol forgot Hello World!");
+    
   }
 }
